4e7f7ea581e6eea92b56c85d1c352d3e16188eed
[Aung-Thu-Hein] <[aungthuhein225k@gmail.com]>
2024-01-19 23:10:06 +0630

    lambda function commit

---
+++ lambdaFunction.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $books = [
+            [
+                'name' => 'Do Androids Dream of Electric Sheep',
+                'author' => 'Philip K. Dick',
+                'releaseYear' => 1968,
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'Project Hail Mary',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2021,
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'The Martion',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2011,
+                'purchaseUrl' => 'http://example.com'
+            ],
+        ];
+
+        //Lambda Function
+        function filter($items, $fn) {
+
+            $filteredItems = [];
+
+            foreach($items as $item) {
+                if($fn($item)) {
+                    $filteredItems[] = $item;
+                }
+            }
+
+            return $filteredItems;
+        };
+
+        $filteredBooks = filter($books, function($book) {
+             return $book['releaseYear'] >= 2011;
+        });
+        
+        //filter by using build in function not to need to use filter function
+        // $filteredBooks = array_filter($books, function($book) {
+        //     return $book['author'] === 'Philip K. Dick';
+        // });
+    ?>
+
+    <ul>
+        <?php foreach($filteredBooks as $book): ?>
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['name']; ?> ( <?= $book['author'] ?>)
+                </a>
+            </li>
+        <?php endforeach ?>
+    </ul>
+</body>
+</html>
